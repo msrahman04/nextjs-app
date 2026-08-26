32c6c7ce84d9ff8e18db041c76a425bb254510c6
Shahin <msrahman.mail@gmail.com>
2026-08-26 18:34:47 +0600

    New change for test

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-1 items-center justify-center bg-zinc-50 dark:bg-black">
       <h1 className="text-5xl font-semibold tracking-tight text-black dark:text-zinc-50">
-        Hello Kubernetes! This is Shahin. New
+        Hello Kubernetes! This is Shahin. New Next.js App with Tailwind CSS and TypeScript.
       </h1>
     </div>
   );
