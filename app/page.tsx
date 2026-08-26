7c1873e0d9449ac4004101da3d17653a415695ab
Shahin <msrahman.mail@gmail.com>
2026-08-26 09:53:56 +0600

    change text from shahin to kubernetes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-1 items-center justify-center bg-zinc-50 dark:bg-black">
       <h1 className="text-5xl font-semibold tracking-tight text-black dark:text-zinc-50">
-        Hello Shahin! Great!
+        Hello Kubernetes!
       </h1>
     </div>
   );
