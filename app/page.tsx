792100b1f24aac731529726e51752779c9744de1
Shahin <msrahman.mail@gmail.com>
2026-08-26 18:27:13 +0600

    Again change

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-1 items-center justify-center bg-zinc-50 dark:bg-black">
       <h1 className="text-5xl font-semibold tracking-tight text-black dark:text-zinc-50">
-        Hello Kubernetes! This is Shahin.
+        Hello Kubernetes! This is Shahin. New
       </h1>
     </div>
   );
